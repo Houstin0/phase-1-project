2be4c755cc8bdfe0023cf7514a6f6e962decc87b
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-05 14:47:11 +0300

    added function to display

--- animeWorld.js
+++ animeWorld.js
@@ -15,7 +15,37 @@ document.addEventListener("DOMContentLoaded",()=>{
       displayCard(anime)
     })
   })
+
+
+
+ //function to display anime cards in container
+ function displayCard(anime) {
+    const cardContainer=document.getElementById("animeContainer")
+    cardContainer.innerHTML=" "
+  
+      const animeElement=document.createElement("div")
+      animeElement.classList.add("animeCard")
     
+      const imageElement=document.createElement("img")
+      imageElement.src=anime.images.jpg.image_url
+      imageElement.alt="Anime pic"
+      animeElement.appendChild(imageElement)
+    
+      const titleElement=document.createElement('h3')
+      titleElement.textContent=anime.title
+      animeElement.appendChild(titleElement)
+  
+      const nextBtn=document.createElement('button')
+      nextBtn.textContent="Next"
+      animeElement.appendChild(nextBtn)
+  
+      cardContainer.appendChild(animeElement)
+  
+  
+  }
+  
+
+
  })
 
 .catch((error) => {
