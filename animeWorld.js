6757443160b17e42240759bd7e1a0d50d3fbb578
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-07 00:18:24 +0300

    added content on both containers

--- animeWorld.js
+++ animeWorld.js
@@ -9,7 +9,6 @@ searchButton.addEventListener('click',(e)=>{
 })
 // function to search for inputed anime in the api and display the results
 function fetchAnime(searchValue) {
-  console.log(searchValue);
   fetch(`https://api.jikan.moe/v4/anime?q=${searchValue}`,{
       method: 'GET'
   })
@@ -18,7 +17,7 @@ function fetchAnime(searchValue) {
    const animes=data.data
      if(animes.length>0){
      const displayResultContainer=document.getElementById("displaySearchResult")
-     displayResultContainer.innerHTML=" "// clear previous content
+     displayResultContainer.innerHTML=" "//add card elements
      animes.forEach(anime=>{
       const animeElement=displayAnimeResult(anime)
       displayResultContainer.appendChild(animeElement)
@@ -43,44 +42,52 @@ function displayAnimeResult(anime){
     imageElement.src=anime.images.jpg.image_url
     imageElement.alt="Anime pic"
     animeElement.appendChild(imageElement)
-  
     const description=document.createElement('p')
     description.textContent=`Description: ${anime.synopsis}`
     animeElement.appendChild(description) 
 
     const infoElement=document.createElement("div")
     infoElement.classList.add("infoCard")
-    
+//add card elements
     const titleElement=document.createElement('h3')
     titleElement.textContent=anime.title_english
     infoElement.appendChild(titleElement)
-
-    const episodesElement=document.createElement('p')
-    episodesElement.textContent=`Episodes: ${anime.episodes}` 
-    infoElement.appendChild(episodesElement)
-
     const statusElement=document.createElement("p")
     statusElement.textContent=`Status: ${anime.status}`
     infoElement.appendChild(statusElement)
-  
-
-
-
-  
-    
+    const episodesElement=document.createElement('p')
+    episodesElement.textContent=`Episodes: ${anime.episodes}` 
+    infoElement.appendChild(episodesElement)
+    const ratedscore=document.createElement("p")
+    ratedscore.textContent=`Rated score: ${anime.score}, scored by:${anime.scored_by}`
+    infoElement.appendChild(ratedscore)
     const durationElement=document.createElement("p")
     durationElement.textContent=`Duration: ${anime.duration}`
     infoElement.appendChild(durationElement)
-
-
-
+    const yearElement=document.createElement("p")
+    yearElement.textContent=`Year: ${anime.year}`
+    infoElement.appendChild(yearElement)
     const airedElement=document.createElement("p")
-    airedElement.textContent=`Aired from: ${anime.aired.string}`
+    airedElement.textContent=`Aired from ${anime.aired.string}`
     infoElement.appendChild(airedElement)
-    
     const ratedElement=document.createElement("p")
     ratedElement.textContent=`Rated:${anime.rating}`
     infoElement.appendChild(ratedElement)
+    const genreElement=document.createElement("p")
+    const genres=anime.genres
+    genres.forEach(genre=>{
+      genreElement.textContent=`Genre: ${genre.name}`
+      infoElement.appendChild(genreElement)
+    })
+    const producersElement=document.createElement("p")
+    const producers=anime.producers
+    producers.forEach(producer=>{
+      producersElement.textContent=`Producers: ${producer.name}`
+      infoElement.appendChild(producersElement)
+    })
+    const backgroundElement=document.createElement("p")
+    backgroundElement.textContent=`Backgound: ${anime.background}`
+    infoElement.appendChild(backgroundElement)
   //add the animeCard to the container
     displayResultContainer.appendChild(animeElement)
     displayResultContainer.appendChild(infoElement)
@@ -113,34 +120,59 @@ function displayAnimeResult(anime){
  //function to display anime cards in container with list
  function displayCard(anime) {
     const cardContainer=document.getElementById("animeContainer")
-    //
+    //add card elements
     cardContainer.innerHTML=" "
   
       const animeElement=document.createElement("div")
       animeElement.classList.add("animeCard")
-    
+    //add card elements
       const imageElement=document.createElement("img")
       imageElement.src=anime.images.jpg.image_url
       imageElement.alt="Anime pic"
       animeElement.appendChild(imageElement)
-    
       const titleElement=document.createElement('h3')
       titleElement.textContent=anime.title
       animeElement.appendChild(titleElement)
-
+      const statusElement=document.createElement("p")
+      statusElement.textContent=`Status: ${anime.status}`
+      animeElement.appendChild(statusElement)
       const episodesElement=document.createElement('p')
       episodesElement.textContent=`Episodes: ${anime.episodes}` 
       animeElement.appendChild(episodesElement)
-  
+      const ratedscore=document.createElement("p")
+      ratedscore.textContent=`Rated score: ${anime.score}, scored by:${anime.scored_by}`
+      animeElement.appendChild(ratedscore)
+      const durationElement=document.createElement("p")
+      durationElement.textContent=`Duration: ${anime.duration}`
+      animeElement.appendChild(durationElement)
+      const yearElement=document.createElement("p")
+      yearElement.textContent=`Year: ${anime.year}`
+      animeElement.appendChild(yearElement)
+      const airedElement=document.createElement("p")
+      airedElement.textContent=`Aired from ${anime.aired.string}`
+      animeElement.appendChild(airedElement)
       const ratingElement=document.createElement('p')
-      ratingElement.textContent=`rating: ${anime.rating}`
+      ratingElement.textContent=`rated: ${anime.rating}`
       animeElement.appendChild(ratingElement)
-    
+      const genreElement=document.createElement("p")
+      const genres=anime.genres
+      genres.forEach(genre=>{
+        genreElement.textContent=`Genre: ${genre.name}`
+        animeElement.appendChild(genreElement)
+      })
+      const producersElement=document.createElement("p")
+      const producers=anime.producers
+      producers.forEach(producer=>{
+        producersElement.textContent=`Producers: ${producer.name}`
+        animeElement.appendChild(producersElement)
+      })
       const descriptionElement=document.createElement('p')
-      descriptionElement.textContent=anime.synopsis
+      descriptionElement.textContent=`Description: ${anime.synopsis}`
       animeElement.appendChild(descriptionElement)
-      
-      
+      const backgroundElement=document.createElement("p")
+      backgroundElement.textContent=`Backgound: ${anime.background}`
+      animeElement.appendChild(backgroundElement)
+
       const likeButton=document.createElement("button")
       likeButton.textContent="Like"
       likeButton.classList.add("likeButton")
@@ -150,8 +182,6 @@ function displayAnimeResult(anime){
         likeButton.style.backgroundColor="red"
         likeButton.textContent="liked"
       })
-  
       cardContainer.appendChild(animeElement)
-     //displayResult.appendChild(animeElement)
   }  
 })
